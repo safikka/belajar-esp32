22752ad49bdbe50352e910057333b8f8525e20fd
mahdy.jannata <mahdy.jannata@delameta.com>
2022-02-17 16:50:21 +0700

    [ADD] Menambahkan kodingan terkait Hello World dengan cara seri (blocking) dan paralel (non blocking)

--- 1-hello-world/src/main.c
+++ 1-hello-world/src/main.c
@@ -1,5 +1,30 @@
 #include <main.h>
 
+void task_Hello1(void* param){
+    while(1){
+        printf("Hello - 1\n");
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+}
+
+void task_Hello2(void* param){
+    while (1){
+        printf("Hello - 2\n");
+        vTaskDelay(2000 / portTICK_RATE_MS);
+    }
+}
+
 void app_main() {
 
+    // NOTE Blocking loopnya
+    // while(1){
+    //     printf("Hello Blocking\n");
+    //     vTaskDelay(500 / portTICK_RATE_MS);
+    // }
+
+
+    // NOTE Biar paralel pake FreeRTOS
+    // parameternya itu (fungsiTask, namaTask, sizeStackTask, paramYgMauDiPassing, taskPriority, taskHandle)
+    xTaskCreate(task_Hello1,"hello1",1024,NULL,1,NULL);
+    xTaskCreate(task_Hello2,"hello2",1024,NULL,2,NULL);
 }
