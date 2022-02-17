9e5aea323b41d8602e18b9d774e85b3e04885bfd
mahdy.jannata <mahdy.jannata@delameta.com>
2022-02-17 15:21:42 +0700

    [ADD] Komit awal perjalanan ESP32

---
+++ 1-hello-world/include/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+// Library bawaan C
+#include <stdio.h>
+
+// Library peripheral
+#include <driver/gpio.h>
+
+// Library FreeRTOS
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+// Library bawaan ESP-IDF
+#include "esp_log.h"
+
+#endif
