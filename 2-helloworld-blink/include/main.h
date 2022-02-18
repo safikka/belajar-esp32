0be42ab8d11d8cd548d24baf27efeced71cf7a96
mahdy.jannata <mahdy.jannata@delameta.com>
2022-02-18 10:24:29 +0700

    [ADD] Hello World + Blink berjalan paralel

---
+++ 2-helloworld-blink/include/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+// Lib bawaan C
+#include <stdio.h>
+
+// Lib peripheral
+#include <driver/gpio.h>
+
+// Lib freeRTOS
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+// Lib bawaan ESP-IDF
+#include <esp_log.h>
+
+#endif
