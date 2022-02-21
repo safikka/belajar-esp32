580d6cff7325b5d459f69f581b3349265296b69f
mahdy.jannata <mahdy.jannata@delameta.com>
2022-02-21 09:43:40 +0700

    [CHANGE] Perubahan di repo 2-hello-world

--- 2-helloworld-blink/src/main.c
+++ 2-helloworld-blink/src/main.c
@@ -1,5 +1,14 @@
 #include <main.h>
 
+void print_Hello(void* param){
+    while(1){
+        ESP_LOGI(TAG,"Hello World!");
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+    
+}
+
 void app_main() {
     
+
 }
